85037076c78d5c78b2c6e3d5627af965b6656760
Clemens Prerovsky <c.prerovsky@gentics.com>
2016-12-06 09:48:52 +0100

    updated package.json

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -1,17 +1,40 @@
-import{MeshAPI} from "./build/MeshAPI";
+import { MeshAPI } from "./build/MeshAPI";
 
 let Mesh = new MeshAPI();
 
-async function test (): Promise<any> {
-    let demo = Mesh.api.project("demo");
+async function test(): Promise<any> {
+    const PARENT_UUID = "8e7b5519386a4380bb5519386a1380f9";
     await Mesh.api.auth.login.post({ username: "admin", password: "admin" });
-    let nodes = await demo.nodes.get({ version: "draft" });
+    let demo = Mesh.api.project("demo");
+    let nodes = await demo.nodes.get({ page: 1, perPage: 300, version: "draft" });
     for (let node of nodes.data) {
-        // node.fields.name += " C";
-        // demo.nodes.nodeUuid(node.uuid).post(node, { version: "draft" });
-        demo.nodes.nodeUuid(node.uuid).get();
+        demo.nodes.nodeUuid(node.uuid).get({ version: "draft"});
     }
+    // for (let j = 0; j < 10000; j++) {
+    //     await demo.nodes.post({
+    //         parentNodeUuid: PARENT_UUID,
+    //         schema: { name: "vehicleImage" },
+    //         language: "en",
+    //         fields:
+    //         {
+    //             name: 'NODE ' + j + ' ' + Date.now(),
+    //         }
+    //     }, { c: j });
+    // }
 }
-test().catch(reason => {
+
+    // adding new nodes
+    // execution time:   78705ms -  1000 nodes
+    // execution time: 1419046ms - 10000 nodes
+    // for (let node of nodes.data) {
+    //     // node.fields.name += " C";
+    //     // demo.nodes.nodeUuid(node.uuid).post(node, { version: "draft" });
+    //     demo.nodes.nodeUuid(node.uuid).get();
+    // }
+
+let timer = Date.now();
+test().then(() => {
+    console.log("execution time: " + (Date.now() - timer) + "ms");
+}).catch(reason => {
     console.log(reason);
 });
